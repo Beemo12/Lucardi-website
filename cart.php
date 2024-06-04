20345bad6056b92b06a1e146b48680775fefa828
Yassine B <yassinemens53@gmail.com>
2024-06-04 14:26:30 +0200

    responsive

--- cart.php
+++ cart.php
@@ -13,8 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $product_id = $_POST['product_id'];
     $product_name = $_POST['product_name'];
     $product_price = $_POST['product_price'];
-
-    // Check if the product is already in the cart
     if (isset($_SESSION['cart'][$product_id])) {
         $_SESSION['cart'][$product_id]['quantity']++;
     } else {
--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// JavaScript to handle image thumbnail click
 document.querySelectorAll('.thumbnails img').forEach(thumb => {
     thumb.addEventListener('click', function() {
         document.querySelector('.main-image').src = this.src;
